f569ebdd0cc2e4695c172769884079eb2085759e
cesar <cesalberca@hotmail.es>
2016-05-06 17:17:41 +0200

    Scripts y prefabs vacíos

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Completed
+{
+	using System.Collections.Generic;		//Allows us to use Lists. 
+	using UnityEngine.UI;					//Allows us to use UI.
+	
+	public class GameManager : MonoBehaviour
+	{
+		
+		void Awake()
+		{
+		
+		}
+		
+		
+		//Update is called every frame.
+		void Update()
+		{
+			
+		}
+		
+	}
+}
+
