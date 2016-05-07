db20b7fa014b02f5baa1b4660a369f76051a1a53
César Alberca <cesalberca@gmail.com>
2016-05-07 21:19:26 +0200

    Base del jugador creada

---
+++ Assets/Scripts/Base.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Base : MonoBehaviour {
+
+    private float posicionX;
+    private float posicionY;
+
+	// Use this for initialization
+	void Start () {
+        this.posicionX = 200;
+        this.posicionY = 200;	
+	}
+
+    public float PosicionX
+    {
+        get { return posicionX; }
+        set { posicionX = value; }
+    }
+
+    public float PosicionY
+    {
+        get { return posicionY; }
+        set { posicionY = value; }
+    }
+}
