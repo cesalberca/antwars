80b91c90cb88a3867baefed7a70ef13e459c5726
César Alberca <cesalberca@gmail.com>
2016-05-07 20:28:59 +0200

    Creación del script de enemigo.

---
+++ Assets/Scripts/Enemigo.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Enemigo : Personaje {
+
+    // Daño que hace el enemigo sobre el personaje.
+    public int danoAtaque = 3;
+
+    // Posición del jugador.
+    private Transform objetivo;
+
+	// Sobreescribimos la clase de start de la superclase.
+	protected override void Start () {
+        //objetivo = GameObject.FindGameObjectsWithTag("Jugador").transform;
+        base.Start();
+	}
+	
+	public void moverEnemigo()
+    {
+        int x = 1;
+        int y = 1;
+
+        intentarMovimiento<Jugador>(x, y);
+    }
+
+    protected override void choqueConObjeto<T>(T componente)
+    {
+        // Comprobado si el enemigo se ha chocado contra el jugador.
+        Jugador jugadorAtacado = componente as Jugador;
+
+        //jugadorAtacado.quitarVida(danoAtaque);
+
+    }
+
+    protected override void mover(Vector2 destino)
+    {
+        throw new NotImplementedException();
+    }
+}
