971916c6f98fc535d1bf3ff856cde814a82cd162
Arturo Rodriguez <arturorodriguezzromero@gmail.com>
2016-05-09 18:51:37 +0200

    Añadida una clase arma
    Poca cosa

---
+++ Assets/Scripts/Arma.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arma : MonoBehaviour {
+
+    public int id;
+    public int dano;
+    public int rango;
+    public int amplitud;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
