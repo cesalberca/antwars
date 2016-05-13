513c36aa41f1ae1c36eb0d4adfc212539726057e
cesar <cesalberca@hotmail.es>
2016-05-13 18:10:24 +0200

    Empezando a añadir métodos y funcionalidad al GestorJuego

--- Assets/Scripts/GestorJuego.cs
+++ Assets/Scripts/GestorJuego.cs
@@ -6,21 +6,50 @@ namespace Completed
 	using System.Collections.Generic;		//Allows us to use Lists. 
 	using UnityEngine.UI;					//Allows us to use UI.
 	
-	public class GameManager : MonoBehaviour
+	public class GestorJuego : MonoBehaviour
 	{
+
+        // Instancia estático del GestorJuego para poder accederla desde otras clases.
+        public static GestorJuego instance = null;
+        // Array de enemigos
+        private List<Enemigo> enemigos;
 		
 		void Awake()
 		{
-		
+            // Este if comprueba si hay una instancia de GestorJuego, si la hay la destruye para evitar que se instancia una nueva.
+            if (instance == null)
+            {
+                instance = this;
+            } else if (instance != this)
+            {
+                Destroy(gameObject);
+            }
+            DontDestroyOnLoad(gameObject);
+            enemigos = new List<Enemigo>();
+            iniciarJuego();
 		}
+
+        void iniciarJuego()
+        {
+            enemigos.Clear();
+        }
 		
 		
 		//Update is called every frame.
 		void Update()
 		{
-			
+            StartCoroutine(moverEnemigos());
 		}
 		
+        IEnumerator moverEnemigos()
+        {
+            for (int i = 0; i < enemigos.Count; i++)
+            {
+                enemigos[i].moverEnemigo();
+                yield return new WaitForSeconds(enemigos[i].velocidad);
+            }
+        }
+
 	}
 }
 
