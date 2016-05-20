9aca646bd487e29f032e66bd770474ceea3b8c4c
Arturo Rodriguez <arturorodriguezzromero@gmail.com>
2016-05-20 16:27:37 +0200

    Un puntero
    *Añadido un script para que haya un cursor que sigue el raton
    *Molaria que dependiendo del arma haya un puntero u otro

---
+++ Assets/Scripts/Armas/Extras/Puntero.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Puntero : MonoBehaviour {
+
+    public Camera camaraPrincipal;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        seguirRaton();
+	}
+
+    void seguirRaton()
+    {
+        this.transform.position = new Vector3(getMousePosition().x, getMousePosition().y, -1);
+    }
+
+    private Vector3 getMousePosition()
+    {
+        Vector3 mousePos = new Vector2(camaraPrincipal.ScreenToWorldPoint(Input.mousePosition).x, camaraPrincipal.ScreenToWorldPoint(Input.mousePosition).y);
+        return mousePos;
+    }
+}
