b5a83cc7a141ee94873031e6063b96e73e4955e7
VicenteRayo <vrs1985@hotmail.com>
2016-05-27 18:17:56 +0200

    Ya va el mapa funcionando algo
    Empiezan a crearse los caminos en el mapa. (mas o menos)

--- Assets/Scripts/Mapa.cs
+++ Assets/Scripts/Mapa.cs
@@ -19,43 +19,77 @@ public class Mapa : MonoBehaviour {
     // Update is called once per frame
     void Update() {
     }
-        void crearMundo() { 
-    
-            arrayTiles = new GameObject[tilesFila, tilesColumna];
+    void crearMundo()
+    {
 
-            int elegidor;
-            Muro muroACrear;
+        arrayTiles = new GameObject[tilesFila, tilesColumna];
+
+        Muro muroACrear;
 
 
 
-            for (int i = 0; i < tilesFila; i++)
+
+        for (int i = 0; i < tilesFila; i++) //Con este doble For creamos el fondo del mapa;
+        {
+            for (int j = 0; j < tilesColumna; j++)
             {
-                for (int j = 0; j < tilesColumna; j++)
-                {
-                    muroACrear = new Muro(j, i);
-                //if (arrayTiles[i, j-1].ge
-                //{
-                elegidor = Random.Range(0, 100);
-                //}
-                //else
-                //{
-                //    elegidor = Random.Range(0, 60);
-                //}
-
-                if (elegidor < porcientoBloque)
-                    {
-                        arrayTiles[i, j] = Instantiate(muro, new Vector3(muroACrear.posicionX, -muroACrear.posicionY, 0), Quaternion.identity) as GameObject;
-                        arrayTiles[i, j].AddComponent<BoxCollider2D>();
-                        arrayTiles[i, j].AddComponent<Muro>();
-                        arrayTiles[i, j].GetComponent<Muro>().posicionX = muroACrear.posicionX;
-                        arrayTiles[i, j].GetComponent<Muro>().posicionY = muroACrear.posicionY;
-                        arrayTiles[i, j].GetComponent<Muro>().vidaMuro = muroACrear.vidaMuro;
-                        arrayTiles[i, j].GetComponent<Muro>().costConstruccion = muroACrear.costConstruccion;
-                        //arrayTiles[i, j].tag = "Default";
-                    }
-                    else { }
-                    arrayTiles[i, j] = Instantiate(fondo, new Vector3(j, -i, 1), Quaternion.identity) as GameObject;//cambiar el resource load por una referencia
-                }
+                arrayTiles[i, j] = Instantiate(fondo, new Vector3(j, -i, 1), Quaternion.identity) as GameObject;//cambiar el resource load por una referencia
             }
         }
+
+        for (int i = 0; i < tilesFila; i++)
+        {
+            for (int j = 0; j < tilesColumna; j++)
+            {
+                muroACrear = new Muro(j, i);
+                arrayTiles[i, j] = Instantiate(muro, new Vector3(muroACrear.posicionX, -muroACrear.posicionY, 0), Quaternion.identity) as GameObject;
+                arrayTiles[i, j].AddComponent<BoxCollider2D>();
+                arrayTiles[i, j].AddComponent<Muro>();
+                arrayTiles[i, j].GetComponent<Muro>().posicionX = muroACrear.posicionX;
+                arrayTiles[i, j].GetComponent<Muro>().posicionY = muroACrear.posicionY;
+                arrayTiles[i, j].GetComponent<Muro>().vidaMuro = muroACrear.vidaMuro;
+                arrayTiles[i, j].GetComponent<Muro>().costConstruccion = muroACrear.costConstruccion;
+            }
+        }
+
+        for (int j = 1; j < tilesColumna - 1; j++)
+        {
+            for (int destruidor = Random.Range(1, 5); destruidor < Random.Range(5, 10); destruidor++)
+            {
+                Destroy(arrayTiles[destruidor, j].transform.gameObject);
+            }
+        }
+        /*for (int j = 1; j < tilesColumna - 1; j++)
+        {
+            for (int destruidor = Random.Range(10, 15); destruidor < Random.Range(15, 20); destruidor++)
+            {
+                Destroy(arrayTiles[destruidor, j].transform.gameObject);
+            }
+        }*/
+        for (int j = 1; j < tilesColumna - 1; j++)
+        {
+            for (int destruidor = Random.Range(20, 25); destruidor < Random.Range(25, 29); destruidor++)
+            {
+                Destroy(arrayTiles[destruidor, j].transform.gameObject);
+            }
+        }
+
+        for (int i = 1; i < tilesFila - 1; i++)
+        {
+            for (int destruidor = Random.Range(5, 10); destruidor < Random.Range(10, 15); destruidor++)
+            {
+                Destroy(arrayTiles[i, destruidor].transform.gameObject);
+            }
+        }
+        for (int i = 1; i < tilesFila-1; i++)
+        {
+            for (int destruidor = Random.Range(15, 20); destruidor < Random.Range(20, 25); destruidor++)
+            {
+                Destroy(arrayTiles[i, destruidor].transform.gameObject);
+            }
+
+        }
+    } 
     }
+     
+ 
