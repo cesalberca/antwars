24f17d21e862292dbd913b87836ef6bd956a427b
Arturo Rodriguez <arturorodriguezzromero@gmail.com>
2016-05-31 16:40:22 +0200

    Motosierra!
    *Script arma melee añadida
    *Añadido un prefab dela motosierra

---
+++ Assets/Scripts/Armas/ArmaMelee.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArmaMelee : ArmaBasica {
+
+	// Use this for initialization
+	void Start () {
+        camaraPrincipal = GameObject.Find("Main Camera").GetComponent<Camera>();
+        jugador = GameObject.Find("Jugador").transform.gameObject;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        moverArma();
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.transform.GetComponent<Personaje>())
+        {
+            coll.transform.GetComponent<Personaje>().bajarVida(this.dano);
+        } else if (coll.transform.GetComponent<Muro>()){
+            coll.transform.GetComponent<Muro>().bajarVida(this.dano);
+        }
+    }
+}
